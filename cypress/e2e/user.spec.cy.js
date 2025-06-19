798f2be63dd6e016c79d7a621fe6adf0d9b17a9e
ViniciusSincariuc <visincariuc@icloud.com>
2025-06-19 00:32:28 -0300

    Adding commands to the my information page

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -13,6 +13,17 @@ const selectorsList = {
 
 }
 
+const myinfoList = {
+  mynameFirst: "[name='firstName']",
+  mynameMiddle: "[name='middleName']",
+  mynameLast: "[name='lastName']",
+  nickName: ".oxd-input--active",
+  employeeId: ".oxd-input--active",
+  otherId: ".oxd-input--active",
+
+}
+
+
   it.only('User Info Update - Success', () => {
 
     cy.visit('/auth/login')
@@ -22,6 +33,13 @@ const selectorsList = {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
     cy.get(selectorsList.myinfoButton).click()
+    cy.get(myinfoList.mynameFirst).clear().type("vinicius")
+    cy.get(myinfoList.mynameMiddle).clear().type("Roberto")
+    cy.get(myinfoList.mynameLast).clear().type("Sincariuc")
+    cy.get(myinfoList.nickName).eq(4).clear().type('Sky')
+    cy.get(myinfoList.employeeId).eq(5).clear().type("User1")
+    cy.get(myinfoList.otherId).eq(6).clear().type("12345678")
+    
   })
 
     it('Login - Fail', () => {
